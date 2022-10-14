46ff097b7e8db36f9a3f20feb55ac9499ccb9118
Fernando Dingler <ferdingler@users.noreply.github.com>
2022-10-14 10:54:46 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Today is Oct 4, 2022. This is the second build
+          Today is Oct 14, 2022. This is the second build
         </h1>
 
         <p className={styles.description}>
